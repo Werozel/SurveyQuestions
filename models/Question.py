9934832e0b5f241dccbdc81a8a3a03c5c6488ab9
Werozel <khaibulaev.mn@gmail.com>
2022-09-26 15:14:06 +0300

    Added some questions and populate script

--- models/Question.py
+++ models/Question.py
@@ -1,6 +1,15 @@
-from sqlalchemy import Column, Integer, VARCHAR
-
+from sqlalchemy import Column, Integer, VARCHAR, Enum
+import enum
 from globals import Base
+
+
+class AnswerType(enum.Enum):
+    YES_NO = 1
+    TEXT = 2
+    RANGE_5 = 3
+    RANGE_10 = 4
+    CUSTOM = 5
+    RANGE_7 = 6
 
 
 class Question(Base):
@@ -9,6 +18,7 @@ class Question(Base):
     id = Column(Integer, primary_key=True)
     raw = Column(VARCHAR, nullable=False)
     transformed = Column(VARCHAR, nullable=True)
+    answer_type = Column(Enum(AnswerType), default=AnswerType.YES_NO)
 
     def __repr__(self):
         return f"{self.id}: {self.raw}"
