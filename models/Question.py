12ff2819b637ac33d3bea2ad48403d28bc476eb2
Werozel <khaibulaev.mn@gmail.com>
2022-09-26 15:38:40 +0300

    Some more questions

--- models/Question.py
+++ models/Question.py
@@ -4,12 +4,17 @@ from globals import Base
 
 
 class AnswerType(enum.Enum):
-    YES_NO = 1
-    TEXT = 2
-    RANGE_5 = 3
-    RANGE_10 = 4
-    CUSTOM = 5
-    RANGE_7 = 6
+    CUSTOM = 0
+    TEXT = 1
+    YES_NO = 2
+    RANGE_3 = 3
+    RANGE_4 = 4
+    RANGE_5 = 5
+    RANGE_6 = 6
+    RANGE_7 = 7
+    RANGE_8 = 8
+    RANGE_9 = 9
+    RANGE_10 = 10
 
 
 class Question(Base):
