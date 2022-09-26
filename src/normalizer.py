1dc328b82efe04fbe7c368e706e8d6880029fdfe
Werozel <khaibulaev.mn@gmail.com>
2022-09-26 22:39:34 +0300

    Some fixes

--- src/normalizer.py
+++ src/normalizer.py
@@ -1,40 +1,31 @@
-from globals import session_factory
+from sqlalchemy.orm import Session
 from pymorphy2 import MorphAnalyzer
 from models.Question import Question
 import re
 import nltk
 
 
-def split_str(s: str) -> list[str]:
-    return re.split("[,.-;!?_: ]", s)
+def __split_str(s: str) -> list[str]:
+    return re.split("[,.\-;!?_: ()»«/]", s)
 
 
-def has_alpha(s: str) -> bool:
+def __has_alpha(s: str) -> bool:
     return any([x.isalpha() for x in s])
 
 
-def tokenize_word(word: str, morph: MorphAnalyzer) -> str:
+def __tokenize_word(word: str, morph: MorphAnalyzer) -> str:
     return morph.parse(word)[0].normal_form
 
 
-def normalize_str(s: str, morph: MorphAnalyzer) -> str:
-    splitted: list[str] = [x.lower() for x in split_str(s) if x and has_alpha(x)]
-    morphed: list[str] = [tokenize_word(x, morph) for x in splitted]
+def __normalize_str(s: str, morph: MorphAnalyzer) -> str:
+    splitted: list[str] = [x.lower() for x in __split_str(s) if x and __has_alpha(x)]
+    stop_words: list[str] = nltk.corpus.stopwords.words('russian')
+    morphed: list[str] = [__tokenize_word(x, morph) for x in splitted if x not in stop_words]
     return ' '.join(morphed)
 
 
-def transform_questions(morph: MorphAnalyzer) -> None:
-    session = session_factory()
+def transform_questions(morph: MorphAnalyzer, session: Session) -> None:
     for question in session.query(Question).all():
-        transformed_question = normalize_str(question.raw, morph)
+        transformed_question = __normalize_str(question.raw, morph)
         question.transformed = transformed_question
         session.add(question)
-    session.commit()
-    session.close()
-
-
-if __name__ == "__main__":
-    nltk.download("punkt")
-    nltk.download("stopwords")
-
-    print(normalize_str("Сделал/сделаю это - то, да! Сё.", MorphAnalyzer()))
